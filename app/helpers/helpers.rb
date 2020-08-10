6b3a7f2b03d974c97eac00352d34daed1f3e7b6e
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-08-10 18:36:06 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,8 +4,8 @@ class Helpers
     @current_user = User.find(session_hash[:user_id])
   end
 
-  def self.is_logged_in?
-    !!session[:user_id]
+  def self.is_logged_in?(session_hash)
+    !!session_hash[:user_id]
   end
   
 end
