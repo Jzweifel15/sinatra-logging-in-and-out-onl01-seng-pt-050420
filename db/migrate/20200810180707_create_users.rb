cb9609e4d6ab2849cd3fd1c1b2b44016ed80bbb9
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-08-10 18:08:34 +0000

    Automatically backed up by Learn

--- db/migrate/20200810180707_create_users.rb
+++ db/migrate/20200810180707_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   
   def change
+    create_table :users do |column|
+      column.string :username
+      column.string :password 
+      
+    end
   end
   
 end
