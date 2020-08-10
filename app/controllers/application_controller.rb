6abb0c104ccfee54197e6433e4dedc01e437aa8e
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-08-10 18:23:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,10 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username], password: params[:password])
-    
+    if @user 
+      session[:user_id] = @user.id 
+      
+    end
   end
 
   get '/account' do
